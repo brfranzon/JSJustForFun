a4649a91d4147ff4e71f0941b1d9c6b8466e2ba1
franzon <brfranzon@gmail.com>
2021-06-09 09:27:38 -0300

    generic type T

--- functions/1-reverseNumber.js
+++ functions/1-reverseNumber.js
@@ -1,17 +1,21 @@
 /** 1. Write a JavaScript function that reverse a number.**/
 // Example x = 32243;
 // Expected Output : 34223
-var reverse = function (N) {
-    var nToString = N.toString();
+function reverse(arg) {
+    var nToString = typeof (arg) === 'string' ? arg : arg.toString();
     var reversedN = '';
     for (var i = nToString.length - 1; i >= 0; i--) {
         reversedN = reversedN + nToString[i];
     }
-    return parseInt(reversedN);
-};
-var N = 32327912929;
+    return reversedN;
+}
+//const N: number | string = 32327912929;
+var N = 32327912929444;
+var sN = '4351250912';
 /** second solution */
-var reverse2 = function (n) { return parseInt(N.toString().split("").reverse().join('')); };
+var reverse2 = function (arg) { return arg.toString().split("").reverse().join(''); };
 console.log("x = " + N);
-console.log("Output = " + reverse(N));
-console.log("Output = " + reverse2(N));
+console.log("Output number 1 = " + reverse(N));
+console.log("Output string 1 = " + reverse(sN));
+console.log("Output number 2 = " + reverse2(N));
+console.log("Output string 2 = " + reverse2(sN));
--- functions/1-reverseNumber.ts
+++ functions/1-reverseNumber.ts
@@ -2,26 +2,32 @@
 // Example x = 32243;
 // Expected Output : 34223
 
-const reverse = (N: number): number => {
+function reverse<T>(arg: T) {
 
-    const nToString = N.toString();
+    const nToString = typeof (arg) === 'string' ? arg : arg.toString();
 
     let reversedN = '';
     for (let i = nToString.length - 1; i >= 0; i--) {
         reversedN = reversedN + nToString[i];
     }
 
-    return parseInt(reversedN);
+    return reversedN;
 }
 
-const N: number = 32327912929;
+//const N: number | string = 32327912929;
+const N: number = 32327912929444;
+const sN: string = '4351250912';
+
 
 
 /** second solution */
 
-const reverse2 = (n: number): number => parseInt(N.toString().split("").reverse().join(''));
+const reverse2 = <T>(arg: T) => arg.toString().split("").reverse().join('');
 
 
 console.log(`x = ${N}`);
-console.log(`Output = ${reverse(N)}`);
-console.log(`Output = ${reverse2(N)}`);
+console.log(`Output number 1 = ${reverse(N)}`);
+console.log(`Output string 1 = ${reverse(sN)}`);
+
+console.log(`Output number 2 = ${reverse2(N)}`);
+console.log(`Output string 2 = ${reverse2(sN)}`);
